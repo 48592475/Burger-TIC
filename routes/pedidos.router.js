95dd8fb597e24b4e570aa12cf4a9fe9afada3c8b
gaelmos <156100424+gaelmos@users.noreply.github.com>
2024-09-17 08:44:50 -0300

    comit(routers)

--- routes/pedidos.router.js
+++ routes/pedidos.router.js
@@ -9,5 +9,13 @@ const router = Router();
 // Si no, Express interpretará "usuario" como un id y no funcionará correctamente
 
 // Recordar utilizar los middleware verifyToken y/o verifyAdmin en las rutas que correspondan
+router.get("/", verifyToken, pedidosController.getPedidos); // Obtener todos los pedidos (requiere autenticación)
+router.get("/usuario/:usId", verifyToken, pedidosController.getPedidosByUser); 
+router.get("/:id", verifyToken, pedidosController.getPedidoById); 
+router.post("/", verifyToken, pedidosController.createPedido); 
+router.put("/:id/aceptar", verifyAdmin, pedidosController.aceptarPedido); 
+router.put("/:id/comenzar", verifyAdmin, pedidosController.comenzarPedido); 
+router.put("/:id/entregar", verifyAdmin, pedidosController.entregarPedido); 
+router.delete("/:id", verifyAdmin, pedidosController.deletePedido);
 
 export default router;
